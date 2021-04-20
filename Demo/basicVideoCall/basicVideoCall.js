dfde62de45679fdce1d6df54170e3607cffb0b7b
iain <iain.cox@leaseplan.com>
2021-04-20 09:51:47 +0200

    Add doc comments.

--- Demo/basicVideoCall/basicVideoCall.js
+++ Demo/basicVideoCall/basicVideoCall.js
@@ -1,12 +1,33 @@
-// create Agora client
+
+/*
+ *  These procedures use Agora Video Call SDK for Web to enable local and remote
+ *  users to join and leave a Video Call channel managed by Agora SDK.
+ */
+
+/*
+ *  Create an {@link https://docs.agora.io/en/Video/API%20Reference/web_ng/interfaces/iagorartcclient.html|AgoraRTCClient} instance.
+ *
+ * @param {string} mode - The {@link https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/clientconfig.html#mode| streaming algorithm} used by Agora SDK.
+ * @param  {string} codec - The {@link https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/clientconfig.html#codec| client codec} used by the browser.
+ */
 var client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 
+/*
+ * Clear the video and audio tracks used by `client` on initiation.
+ */
 var localTracks = {
   videoTrack: null,
   audioTrack: null
 };
+
+/*
+ * On initiation no users are connected.
+ */
 var remoteUsers = {};
-// Agora client options
+
+/*
+ * On initiation. `client` is not attached to any project or channel for any specific user.
+ */
 var options = {
   appid: null,
   channel: null,
@@ -14,7 +35,10 @@ var options = {
   token: null
 };
 
-// the demo can auto join channel with params in url
+/*
+ * When this page is called with parameters in the URL, this procedure
+ * attempts to join a Video Call channel using those parameters.
+ */
 $(() => {
   var urlParams = new URL(location.href).searchParams;
   options.appid = urlParams.get("appid");
@@ -28,6 +52,11 @@ $(() => {
   }
 })
 
+/*
+ * When a user clicks Join or Leave in the HTML form, this procedure gathers the information
+ * entered in the form and calls join asynchronously. The UI is updated to match the options entered
+ * by the user.
+ */
 $("#join-form").submit(async function (e) {
   e.preventDefault();
   $("#join").attr("disabled", true);
@@ -49,34 +78,43 @@ $("#join-form").submit(async function (e) {
   }
 })
 
+/*
+ * Called when a user clicks Leave in order to exit a channel.
+ */
 $("#leave").click(function (e) {
   leave();
 })
 
+/*
+ * Join a channel, then create local video and audio tracks and publish them to the channel.
+ */
 async function join() {
 
-  // add event listener to play remote tracks when remote user publishs.
+  // Add an event listener to play remote tracks when remote user publishes.
   client.on("user-published", handleUserPublished);
   client.on("user-unpublished", handleUserUnpublished);
 
-  // join a channel and create local tracks, we can use Promise.all to run them concurrently
+  // Join a channel and create local tracks. Best practice is to use Promise.all and run them concurrently.
   [ options.uid, localTracks.audioTrack, localTracks.videoTrack ] = await Promise.all([
-    // join the channel
+    // Join the channel.
     client.join(options.appid, options.channel, options.token || null),
-    // create local tracks, using microphone and camera
+    // Create tracks to the local microphone and camera.
     AgoraRTC.createMicrophoneAudioTrack(),
     AgoraRTC.createCameraVideoTrack()
   ]);
-  
-  // play local video track
+
+  // Play the local video track to the local browser and update the UI with the user ID.
   localTracks.videoTrack.play("local-player");
   $("#local-player-name").text(`localVideo(${options.uid})`);
 
-  // publish local tracks to channel
+  // Publish the local video and audio tracks to the channel.
   await client.publish(Object.values(localTracks));
   console.log("publish success");
 }
 
+/*
+ * Stop all local and remote tracks then leave the channel.
+ */
 async function leave() {
   for (trackName in localTracks) {
     var track = localTracks[trackName];
@@ -87,7 +125,7 @@ async function leave() {
     }
   }
 
-  // remove remote users and player views
+  // Remove remote users and player views.
   remoteUsers = {};
   $("#remote-playerlist").html("");
 
@@ -100,6 +138,13 @@ async function leave() {
   console.log("client leaves channel success");
 }
 
+
+/*
+ * Add the local use to a remote channel.
+ *
+ * @param  {IAgoraRTCRemoteUser} user - The {@link  https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/iagorartcremoteuser.html| remote user} to add.
+ * @param {trackMediaType - The {@link https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/itrack.html#trackmediatype | media type} to add.
+ */
 async function subscribe(user, mediaType) {
   const uid = user.uid;
   // subscribe to a remote user
@@ -120,12 +165,23 @@ async function subscribe(user, mediaType) {
   }
 }
 
+/*
+ * Add a user who has subscribed to the live channel to the local interface.
+ *
+ * @param  {IAgoraRTCRemoteUser} user - The {@link  https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/iagorartcremoteuser.html| remote user} to add.
+ * @param {trackMediaType - The {@link https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/itrack.html#trackmediatype | media type} to add.
+ */
 function handleUserPublished(user, mediaType) {
   const id = user.uid;
   remoteUsers[id] = user;
   subscribe(user, mediaType);
 }
 
+/*
+ * Remove the user specified from the channel in the local interface.
+ *
+ * @param  {string} user - The {@link  https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/iagorartcremoteuser.html| remote user} to remove.
+ */
 function handleUserUnpublished(user) {
   const id = user.uid;
   delete remoteUsers[id];
